861ec36c683e0e6c76a1268b714a98ff838ef768
Andres Duarte <andresduarte010@gmail.com>
2016-10-28 00:57:29 +0000

    scrape_periods_page class method works, used strings instead of regex for now

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,30 +6,33 @@ class Scraper
 
   def self.scrape_periods_page(periods_url)
 
-    doc = Nokogiri::HTML(open(profile_url))
+    doc = Nokogiri::HTML(open(periods_url))
 
     periods = []
 
-    doc.css(".div-section-category-mobile a").each do |period|
+    doc.css("div.div-section-category div.div-section-category-mobile a").each do |period|
       title = period.attribute("title").text
 
-      if title.include?("1960-1969")
-        periods << {period: title.slice("Abstracts "), link: period.attribute("href").value}
-      elsif title.include?("1970-1979")
-        periods << {period: title.slice("Abstracts "), link: period.attribute("href").value}
-      elsif title.include?("1980-1984")
-        periods << {period: title.slice("Abstracts "), link: period.attribute("href").value}
-      elsif title.include?("1985-1989")
-        periods << {period: title.slice("Abstracts "), link: period.attribute("href").value}
-      elsif title.include?("1990-1994")
-        periods << {period: title.slice("Abstracts "), link: period.attribute("href").value}
-      elsif title.include?("1995-1999")
-        periods << {period: title.slice("Abstracts "), link: period.attribute("href").value}
-      elsif title.include?("2000-2004")
-        periods << {period: title.slice("Abstracts "), link: period.attribute("href").value}
+      if title.include?("1960")
+        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
+      elsif title.include?("1970")
+        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
+      elsif title.include?("1980")
+        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
+      elsif title.include?("1985")
+        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
+      elsif title.include?("1990")
+        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
+      elsif title.include?("1995")
+        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
+      elsif title.include?("2000")
+        periods << {period: title.gsub("\u201320", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
       elsif title.include?("2005")
-        periods << {period: title.slice("Abstracts "), link: period.attribute("href").value}
-
-
+        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
+      end
+    end
+    periods
+    binding.pry
+  end
 
 end
