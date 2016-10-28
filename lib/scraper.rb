d9f943ee0f9350b5ca366115f325cdbae42ac165
Andres Duarte <andresduarte010@gmail.com>
2016-10-28 02:30:43 +0000

    changed from periods to subjects method renamed to ::scrape_subjects_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,35 +4,60 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_periods_page(periods_url)
-
+  def self.scrape_subjects_page(periods_url)
     doc = Nokogiri::HTML(open(periods_url))
 
-    periods = []
+    subjects = []
 
-    doc.css("div.div-section-category div.div-section-category-mobile a").each do |period|
-      title = period.attribute("title").text
+    doc.css("div.div-section-category div.div-section-category-mobile a").each do |subject|
+      name = subject.attribute("title").text
 
-      if title.include?("1960")
-        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
-      elsif title.include?("1970")
-        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
-      elsif title.include?("1980")
-        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
-      elsif title.include?("1985")
-        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
-      elsif title.include?("1990")
-        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
-      elsif title.include?("1995")
-        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
-      elsif title.include?("2000")
-        periods << {period: title.gsub("\u201320", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
-      elsif title.include?("2005")
-        periods << {period: title.gsub("\u201319", "-").gsub("Abstracts ", ""), link: period.attribute("href").value}
+      case name
+      when "Aeroplanes"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Children"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Women"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Potraits & People"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Mother and Child"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Nudes"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Landscapes"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Clouds"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Everyday Life"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Families"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Buildings"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Candles"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      when "Skulls"
+        subjects << {subject: name, link: subject.attribute("href").value}
+      else
       end
     end
-    periods
+    subjects
+    binding.pry
+  end
+
+  def self.scrape_period_page(period_url)
+    doc = Nokogiri::HTML(open(period_url))
+
+    paintings = []
+
+    doc.css(".a-thumb-link").each do |painting|
+      paintings << {name: painting.css("span.span-painting-title2").text, link: painting.attribute("href").value }
+    end
+    paintings
     binding.pry
   end
 
 end
+
+Scraper.scrape_subjects_page("https://www.gerhard-richter.com/en/art/paintings")
