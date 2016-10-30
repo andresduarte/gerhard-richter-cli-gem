039ceaa9e757314f6e8a5e01900d2fa0f9c2e710
Andres Duarte <andresduarte010@gmail.com>
2016-10-30 22:31:55 +0000

    minor change: swapped out @@all for self.all

--- lib/painting.rb
+++ lib/painting.rb
@@ -1,13 +1,12 @@
 class Painting
 
   attr_accessor :name, :medium, :year, :size, :price, :painting_url, :artist
-
   @@all = []
 
   def initialize(attributes_hash)
     add_painting_attributes(attributes_hash)
     painting.artist = Artist
-    @@all << self
+    self.all << self
   end
 
   def self.all
@@ -24,9 +23,7 @@ class Painting
   end
 
   def add_painting_attributes(attributes_hash)
-    attributes_hash.each do |key, value|
-      self.send("#{key}=", value)
-    end
+    attributes_hash.each {|key, value| self.send("#{key}=", value)}
     self
   end
 
